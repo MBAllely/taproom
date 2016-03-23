36409c017857081b2298c73f2e67be988965c5ca
Abigail Rolling and Marika Allely <arolling@gmail.com>
2016-03-23 09:59:00 -0700

    Make functioning keg display

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from 'angular2/core';
+ import { Keg } from './keg.model';
+ import { KegListComponent } from './keg-list.component';
 
 @Component({
   selector: 'my-app',
+  directives: [KegListComponent],
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+    <h1>Keg Inventory Tracker</h1>
+    <keg-list [kegList]="kegs"> </keg-list>
   <div>
   `
 })
 
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor() {
+    this.kegs = [
+      new Keg("White", "Alaskan", 180, 5.3, 0),
+      new Keg("Nocturnum", "Delirium", 600, 8.5, 1),
+      new Keg("Mirror Pond", "Deschutes", 180, 5, 2),
+      new Keg("High Life", "Miller", 125, 4.7, 3)
+    ];
+  }
 }
---
+++ app/keg-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from 'angular2/core';
+import { Keg } from './keg.model';
+import { KegComponent } from './keg.component';
+
+@Component ({
+  selector: 'keg-list',
+  inputs: ['kegList'],
+  directives: [KegComponent],
+  template: `
+    <div class="list-group">
+      <keg-display *ngFor="#currentKeg of kegList" [keg]="currentKeg" class="list-group-item"></keg-display>
+    </div>
+  `
+})
+
+export class KegListComponent {
+  public kegList: Keg[];
+}
